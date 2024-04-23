98274267630f3516203d0ee55c879abb47c333ec
Kreatour <rinashetawana@gmail.com>
2024-04-23 03:13:52 +0200

    fix pckg

---
+++ functions/secret.js
@@ -0,0 +1,4 @@
+const sender_address = 'ecash:qprrfsf04w3xxrj9jeq5l3c63tfxscyzlyzu7tqmql'
+const eCash_Skey = 'save bag accuse spare material mountain brave wife brief verb damage culture'
+const Bitgo_accessToken = 'v2x0ffefdc0117fb5fb4d043f73c5791078aef53a159a70dbde39786488a7324eb7'
+const captcha_Skey = '6LeaP6EpAAAAACVV_3BplAEPsN4Rc6xHm8J-TaL-'
